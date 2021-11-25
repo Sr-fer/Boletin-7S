fd98036e489814a734226d0aa891bc94be683191
Sr-Fer <ffernandezmartinez@danielcastelao.org>
2021-11-25 17:14:14 +0100

    Second Commit

--- Ejercicio 2/array.js
+++ Ejercicio 2/array.js
@@ -1,6 +1,6 @@
 function items() {
     var Items = [lavadora, pc, movil, microondas]
-    var Random = Items[Math.floor(Math.Random() * Items.length)]
+    var Random = Items[Math.floor(Math.random() * Items.length)]
 
 
     return Random
