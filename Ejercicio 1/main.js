ff1561159989e8b34f39b8b5f5c41941d25f5340
Fer <92371342+Sr-fer@users.noreply.github.com>
2021-11-25 22:30:23 +0100

    Add files via upload

--- Ejercicio 1/main.js
+++ Ejercicio 1/main.js
@@ -3,13 +3,12 @@ function main() {
   dato = datos()
   var intentos = 0
   var jugar_partida = prompt("Quieres Jugar a Mastermind? 1:Si 2:No")
-  alert_output+("Tienes que adiviar los cinco numero en sus posiciones, si el número esta en la combinación se marcara con una X, si además la posición es correcta será O")
-  alert(dato)
+  alert_output + ("Tienes que adiviar los cinco numero en sus posiciones, si el número esta en la combinación se marcara con una X, si además la posición es correcta será O")
   do {
     (isRunning = true)
     if (jugar_partida == 1) {
       jugador = array_jugador()
-      comprobador(jugador) 
+      comprobador(jugador)
       intentos++
       alert_output("Llevas " + intentos + " Intentos")
     }
